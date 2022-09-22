3c91164fdef207653aa6d4414dec8a9ff0ae5554
wonjin <wonjine7@naver.com>
2022-09-22 20:02:33 +0900

    useParams: URL의 파라미터값을 읽어오기

--- router-example/src/page/ProductDetailPage.js
+++ router-example/src/page/ProductDetailPage.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetailPage = () => {
+  const {id} = useParams();
+  
+
   return (
     <div>
-      <h1>Show ProductDetailPage</h1>
+      <h1>Show ProductDetailPage {id}</h1>
     </div>
   )
 }
