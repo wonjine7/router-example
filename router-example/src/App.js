f6b8ed9050d1c2768b8f44ee15721d5255e44b83
wonjin <wonjine7@naver.com>
2022-09-25 20:42:40 +0900

    Redirect : 페이지를 보호하는 법

--- router-example/src/App.js
+++ router-example/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
+import LoginPage from './page/LoginPage';
+import UserPage from './page/UserPage';
 
 function App() {
+  const [authenticate,setauthenticate]=useState(false);
+
+  const PrivateRoute = () =>{
+    return authenticate == true?<UserPage/>:<Navigate to="/login"/>
+  }
+
   return (
     <div>
       <Routes>
@@ -15,6 +24,8 @@ function App() {
         <Route path='/products' element={<ProductPage/>}/>
         <Route path='/products/:id' element={<ProductDetailPage/>}/> 
         {/*Restful Route -  /products라는 url하나로 여러가지의 액션을 할 수 있게된다.*/}
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='user' element={<PrivateRoute/>}/>
       </Routes>
       
     </div>
